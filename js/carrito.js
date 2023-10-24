6fed443991113de677631c3a7d8a77b5af2950fd
farinaceleste <farinaceleste@gmail.com>
2023-10-24 17:50:01 -0300

    probando js - fallando

--- js/carrito.js
+++ js/carrito.js
@@ -10,29 +10,69 @@ btnCart.addEventListener("click", () => {
 	containerCartProducts.classList.toggle('hidden-cart');
 });
 
-
-
 //----------------------------------------------------------------------------------------------
 
 
 const rowProduct = document.querySelector(".row.product");
-const productsList = document.querySelector (".contenedor-item")
+const productsList = document.querySelector(".contenedor-item")
 
 let allProducts = [];
+const valortotal = document.querySelector('.total-pagar');
+const countProducts = document.querySelector('#contador-productos');
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
 
 productsList.addEventListener('click', e => {
 
-	if (e.target.classList.contains('btn-add-cart'));
-	const Product = e.target.parentElement;
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
 
-	const infoProduct = {
-		quantity: 1,
-		title: Product.querySelector('h4').textContent,
-		price: Product.querySelector('span').textContent
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h4').textContent,
+			price: product.querySelector('span').textContent
+		}
+
+		allProducts = [...allProducts, infoProduct]
+
+		showHTML();
 	}
-
-
-
-	console.log (infoProduct);
 })
 
+// funcion para mostrar productos en carrito
+
+const showHTML = () => {
+
+	rowProduct.innerHTML = '';
+
+	allProducts.forEach(product => {	
+		const containerProduct = document.createElement('div');
+		containerProduct.classList.add('cart-product');
+		containerProduct.innerHTML = `
+
+		<div class="cart-product">
+		<div class="info-cart-product">
+			<span class="cantidad-producto-carrito">${product.quantity}</span>
+			<p class="titulo-producto-carrito">${product.title}</p>
+			<span class="precio-producto-carrito">${product.price}</span>
+			<div><i class="bi bi-x-lg container-cart-icon"></i></div>
+		</div>
+	</div>
+		
+	<div class="count-products">
+		<span id="contador-productos">0</span>
+	</div>
+</div>
+<div class="container-cart-products hidden-cart hidden">
+<div class="row-product">
+	`
+		rowProduct.append(containerProduct);
+
+		total =
+			total.parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+	});
+
+	valorTotal.innerText = `$${total}`;
+	countProducts.innerText = TotalOfProducts;
+};
